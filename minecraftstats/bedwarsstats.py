3120ab91dd1e7fc9d289585b317e6cf2d2eda86f
JustBennnnn <justben009@gmail.com>
2021-09-19 14:48:19 +0100

    created stats model

--- minecraftstats/bedwarsstats.py
+++ minecraftstats/bedwarsstats.py
@@ -8,33 +8,98 @@ __all__ = [
     "OverallBedwarsStats"
 ]
 
-class OverallBedwarsStats(BaseModel):
+game_modes = [
+    "eight_one",
+    "eight_two",
+    "four_four",
+    "two_four",
+    "four_three"
+]
+
+def filter_kwargs(_prefix, _suffix, _game_mode, **kwargs):
+    """Filter the kwargs to the specific gamemode and remove any prefixes or suffixes."""
+    global game_modes
+
+    if _game_mode == "":
+        kwargs = {k: v for k, v in kwargs.items() if game_modes[0] not in k 
+                    and game_modes[1] not in k and game_modes[2] not in k 
+                    and game_modes[3] not in k and game_modes[4] not in k}
+    else:
+        kwargs = {k: v for k, v in kwargs.items() if _game_mode in k}
+   
+    ps_kwargs = {}
+    prefix_kwargs = {}
+    suffix_kwargs = {}
+    extra_kwargs = {}
+
+    completed_values = []
+    if _prefix != "" and _suffix != "":
+        for k, v in kwargs.items():
+            if k not in completed_values:
+
+                if k.find(_prefix) == 0 and k.rfind(_suffix) != -1 and k.rfind(_suffix)+len(_suffix) == len(k):
+                    ps_kwargs[k[len(_prefix):len(k)-len(_suffix)]] = v
+                    completed_values.append(k)
+
+    if _prefix != "":
+        for k, v in kwargs.items():
+            if k not in completed_values:
+                
+                if k.find(_prefix) == 0:
+                    prefix_kwargs[k[len(_prefix):len(k)]] = v
+                    completed_values.append(k)
+
+    if _suffix != "":
+        for k, v in kwargs.items():
+            if k not in completed_values:
+
+                if k.rfind(_suffix) != -1 and k.rfind(_suffix)+len(_suffix) == len(k):
+                    suffix_kwargs[k[0:k.rfind(_suffix)]] = v
+
+    for k, v in kwargs.items():
+        if k not in completed_values:
+            extra_kwargs[k] = v
+
+    kwargs = {**ps_kwargs, **prefix_kwargs, **suffix_kwargs, **extra_kwargs}
+    return kwargs
+
+class StatsModel(BaseModel):
+    """Base Model for other classes."""
+    _prefix: str = ""
+    _suffix: str = ""
+    _game_mode: str = ""
+    def __init__(self, **kwargs):
+        super().__init__(**filter_kwargs(self._prefix, self._suffix, self._game_mode, **kwargs))
+
+class OverallBedwarsStats(StatsModel):
     """Overall bedwars stats."""
-    games_played: int = Field(alias="games_played_bedwars")
-    beds_lost: int = Field(alias="beds_lost_bedwars")
-    coins: int = Field(alias="coins")
-    deaths: int = Field(alias="deaths_bedwars")
-    diamonds_collected: int = Field(alias="diamond_resources_collected_bedwars")
-    final_deaths: int = Field(alias="final_deaths_bedwars")
-    gold_collected: int = Field(alias="gold_resources_collected_bedwars")
-    iron_collected: int = Field(alias="iron_resources_collected_bedwars")
-    losses: int = Field(alias="losses_bedwars")
-    total_resources_collected: int = Field(alias="resources_collected_bedwars")
-    void_deaths: int = Field(alias="void_deaths_bedwars")
-    void_final_deaths: int = Field(alias="void_final_deaths_bedwars")
-    entity_final_deaths: int = Field(alias="entity_attack_final_deaths_bedwars")
-    emeralds_collected: int = Field(alias="emerald_resources_collected_bedwars")
-    entity_deaths: int = Field(alias="entity_attack_deaths_bedwars")
-    entity_kills: int = Field(alias="entity_attack_kills_bedwars")
-    kills: int = Field(alias="kills_bedwars")
-    beds_broken: int = Field(alias="beds_broken_bedwars")
-    fall_damage_deaths: int = Field(alias="fall_deaths_bedwars")
+    _suffix: str = "_bedwars"
+
+    games_played: int
+    beds_lost: int
+    coins: int
+    deaths: int
+    diamonds_collected: int = Field(alias="diamond_resources_collected")
+    final_deaths: int
+    gold_collected: int = Field(alias="gold_resources_collected")
+    iron_collected: int = Field(alias="iron_resources_collected")
+    losses: int
+    total_resources_collected: int = Field(alias="resources_collected")
+    void_deaths: int
+    void_final_deaths: int
+    entity_final_deaths: int = Field(alias="entity_attack_final_deaths")
+    emeralds_collected: int = Field(alias="emerald_resources_collected")
+    entity_deaths: int = Field(alias="entity_attack_deaths")
+    entity_kills: int = Field(alias="entity_attack_kills")
+    kills: int
+    beds_broken: int
+    fall_damage_deaths: int = Field(alias="fall_deaths")
     current_winstreak: int = Field(alias="winstreak")
-    entity_final_kills: int = Field(alias="entity_attack_final_kills_bedwars")
-    final_kills: int = Field(alias="final_kills_bedwars")
-    wins: int = Field(alias="wins_bedwars")
-    void_kills: int = Field(alias="void_kills_bedwars")
-    void_final_kills: int = Field(alias="void_final_kills_bedwars")
-    fall_damage_kills: int = Field(alias="fall_kills_bedwars")
-    fall_damage_final_deaths: int = Field(alias="fall_final_deaths_bedwars")
-    fall_damage_final_kills: int = Field(alias="fall_final_kills_bedwars")
+    entity_final_kills: int = Field(alias="entity_attack_final_kills")
+    final_kills: int
+    wins: int
+    void_kills: int
+    void_final_kills: int
+    fall_damage_kills: int = Field(alias="fall_kills")
+    fall_damage_final_deaths: int = Field(alias="fall_final_deaths")
+    fall_damage_final_kills: int = Field(alias="fall_final_kills")
--- minecraftstats/duelstats.py
+++ minecraftstats/duelstats.py
@@ -14,6 +14,66 @@ __all__ = [
     "SkyWarsDuelStats"
 ]
 
+game_modes = [
+    "classic",
+    "op",
+    "uhc",
+    "sumo",
+    "bridge",
+    "sw"
+]
+
+def filter_kwargs(_prefix, _suffix, _game_mode, **kwargs):
+    """Filter the kwargs to the specific gamemode and remove any prefixes or suffixes."""
+    global game_modes
+
+    if _game_mode == "":
+        kwargs = {k: v for k, v in kwargs.items() if game_modes[0] not in k 
+                    and game_modes[1] not in k and game_modes[2] not in k 
+                    and game_modes[3] not in k and game_modes[4] not in k
+                    and game_modes[5] not in k}
+    else:
+        kwargs = {k: v for k, v in kwargs.items() if _game_mode in k}
+   
+    ps_kwargs = {}
+    prefix_kwargs = {}
+    suffix_kwargs = {}
+
+    completed_values = []
+    if _prefix != "" and _suffix != "":
+        for k, v in kwargs.items():
+            if k not in completed_values:
+
+                if k.find(_prefix) == 0 and k.rfind(_suffix) != -1 and k.rfind(_suffix)+len(_suffix) == len(k):
+                    ps_kwargs[k[len(_prefix):len(k)-len(_suffix)]] = v
+                    completed_values.append(k)
+
+    if _prefix != "":
+        for k, v in kwargs.items():
+            if k not in completed_values:
+                
+                if k.find(_prefix) == 0:
+                    prefix_kwargs[k[len(_prefix):len(k)]] = v
+                    completed_values.append(k)
+
+    if _suffix != "":
+        for k, v in kwargs.items():
+            if k not in completed_values:
+
+                if k.rfind(_suffix) != -1 and k.rfind(_suffix)+len(_suffix) == len(k):
+                    suffix_kwargs[k[0:k.rfind(_suffix)]] = v
+
+    kwargs = {**ps_kwargs, **prefix_kwargs, **suffix_kwargs}
+    return kwargs
+
+class StatsModel(BaseModel):
+    """Base Model for other classes."""
+    _prefix: str = ""
+    _suffix: str = ""
+    _game_mode: str = ""
+    def __init__(self, **kwargs):
+        super().__init__(**filter_kwargs(self._prefix, self._suffix, self._game_mode, **kwargs))
+
 class OverallDuelStats(BaseModel):
     """Overall duel stats.
     
@@ -24,127 +84,151 @@ class OverallDuelStats(BaseModel):
     def recent_games(self) -> list[str]:
         return self.recent_games_object.lower().split("#")
     games_played: int = Field(alias="games_played_duels")
-    current_winstreak: int = Field(alias="current_winstreak")
-    bow_shots: int = Field(alias="bow_shots")
-    bow_hits: int = Field(alias="bow_hits")
-    coins: int = Field(alias="coins")
-    damage_dealt: int = Field(alias="damage_dealt") 
-    deaths: int = Field(alias="deaths")
-    health_regenerated: int = Field(alias="health_regenerated")
-    losses: int = Field(alias="losses")
-    melee_hits: int = Field(alias="melee_hits")
-    melee_swings: int = Field(alias="melee_swings")
+    current_winstreak: int
+    bow_shots: int
+    bow_hits: int
+    coins: int
+    damage_dealt: int
+    deaths: int
+    health_regenerated: int
+    losses: int
+    melee_hits: int
+    melee_swings: int
     best_winstreak: int = Field(alias="best_overall_winstreak")
-    kills: int = Field(alias="kills")
-    wins: int = Field(alias="wins")
-    blocks_placed: int = Field(alias="blocks_placed")
-    golden_apples_eaten: int = Field(alias="golden_apples_eaten")
-    goals: int = Field(alias="goals")
+    kills: int
+    wins: int
+    blocks_placed: int
+    golden_apples_eaten: int
+    goals: int
 
-class ClassicDuelStats(BaseModel):
+class ClassicDuelStats(StatsModel):
     """Classic duel stats.
     
     Any stats to do with hearts are measured in halves.
     """
-    current_winstreak: int = Field(alias="current_winstreak_mode_classic_duel")
-    bow_shots: int = Field(alias="classic_duel_bow_shots")
-    bow_hits: int = Field(alias="classic_duel_bow_hits")
-    damage_dealt: int = Field(alias="classic_duel_damage_dealt")
-    deaths: int = Field(alias="classic_duel_deaths")
-    health_regenerated: int = Field(alias="classic_duel_health_regenerated")
-    losses: int = Field(alias="classic_duel_losses")
-    melee_hits: int = Field(alias="classic_duel_melee_hits")
-    melee_swings: int = Field(alias="classic_duel_melee_swings")
-    games_played: int = Field(alias="classic_duel_rounds_played")
-    best_winstreak: int = Field(alias="best_winstreak_mode_classic_duel")
-    kills: int = Field(alias="classic_duel_kills")
-    wins: int = Field(alias="classic_duel_wins")
+    _prefix: str = "classic_duel_"
+    _suffix: str = "_mode_classic_duel"
+    _game_mode: str = "classic_duel"
 
-class OPDuelStats(BaseModel):
+    current_winstreak: int
+    bow_shots: int
+    bow_hits: int
+    damage_dealt: int
+    deaths: int
+    health_regenerated: int
+    losses: int
+    melee_hits: int
+    melee_swings: int
+    games_played: int = Field(alias="rounds_played")
+    best_winstreak: int
+    kills: int
+    wins: int
+
+class OPDuelStats(StatsModel):
     """OP duel stats.
 
     Any stats to do with hearts are measured in halves.
     """
-    current_winstreak: int = Field(alias="current_winstreak_mode_op_duel")
-    damage_dealt: int = Field(alias="op_duel_damage_dealt")
-    deaths: int = Field(alias="op_duel_deaths")
-    health_regenerated: int = Field(alias="op_duel_health_regenerated")
-    losses: int = Field(alias="op_duel_losses")
-    melee_hits: int = Field(alias="op_duel_melee_hits")
-    melee_swings: int = Field(alias="op_duel_melee_swings")
-    games_played: int = Field(alias="op_duel_rounds_played")
-    best_winstreak: int = Field(alias="best_winstreak_mode_op_duel")
-    kills: int = Field(alias="op_duel_kills")
-    wins: int = Field(alias="op_duel_wins")
+    _prefix: str = "op_duel_"
+    _suffix: str = "_mode_op_duel"
+    _game_mode: str = "op_duel"
 
-class UHCDuelStats(BaseModel):
+    current_winstreak: int
+    damage_dealt: int
+    deaths: int
+    health_regenerated: int
+    losses: int
+    melee_hits: int
+    melee_swings: int
+    games_played: int = Field(alias="rounds_played")
+    best_winstreak: int
+    kills: int
+    wins: int
+
+class UHCDuelStats(StatsModel):
     """UHC duel stats.
 
     Any stats to do with hearts are measured in halves.
     """
-    current_winstreak: int = Field(alias="current_winstreak_mode_uhc_duel")
-    bow_shots: int = Field(alias="uhc_duel_bow_shots")
-    bow_hits: int = Field(alias="uhc_duel_bow_hits")
-    damage_dealt: int = Field(alias="uhc_duel_damage_dealt")
-    deaths: int = Field(alias="uhc_duel_deaths")
-    health_regenerated: int = Field(alias="uhc_duel_health_regenerated")
-    losses: int = Field(alias="uhc_duel_losses")
-    melee_hits: int = Field(alias="uhc_duel_melee_hits")
-    melee_swings: int = Field(alias="uhc_duel_melee_swings")
-    games_played: int = Field(alias="uhc_duel_rounds_played")
-    best_winstreak: int = Field(alias="best_winstreak_mode_uhc_duel")
-    kills: int = Field(alias="uhc_duel_kills")
-    wins: int = Field(alias="uhc_duel_wins")
-    blocks_placed: int = Field(alias="uhc_duel_blocks_placed")
-    golden_apples_eaten: int = Field(alias="uhc_duel_golden_apples_eaten")
+    _prefix: str = "uhc_duel_"
+    _suffix: str = "_mode_uhc_duel"
+    _game_mode: str = "uhc_duel"
 
-class SumoDuelStats(BaseModel):
+    current_winstreak: int
+    bow_shots: int
+    bow_hits: int
+    damage_dealt: int
+    deaths: int
+    health_regenerated: int
+    losses: int
+    melee_hits: int
+    melee_swings: int
+    games_played: int = Field(alias="rounds_played")
+    best_winstreak: int
+    kills: int
+    wins: int
+    blocks_placed: int
+    golden_apples_eaten: int
+
+class SumoDuelStats(StatsModel):
     """Sumo duel stats."""
-    current_winstreak: int = Field(alias="current_winstreak_mode_sumo_duel")
-    deaths: int = Field(alias="sumo_duel_deaths")
-    losses: int = Field(alias="sumo_duel_losses")
-    melee_hits: int = Field(alias="sumo_duel_melee_hits")
-    melee_swings: int = Field(alias="sumo_duel_melee_swings")
-    games_played: int = Field(alias="sumo_duel_rounds_played")
-    best_winstreak: int = Field(alias="best_winstreak_mode_sumo_duel")
-    kills: int = Field(alias="sumo_duel_kills")
-    wins: int = Field(alias="sumo_duel_wins")
+    _prefix: str = "sumo_duel_"
+    _suffix: str = "_mode_sumo_duel"
+    _game_mode: str = "sumo_duel"
 
-class BridgeDuelStats(BaseModel):
+    current_winstreak: int
+    deaths: int
+    losses: int
+    melee_hits: int
+    melee_swings: int
+    games_played: int = Field(alias="rounds_played")
+    best_winstreak: int
+    kills: int
+    wins: int
+
+class BridgeDuelStats(StatsModel):
     """Bridge duel stats.
 
     Any stats to do with hearts are measured in halves.
     """
-    current_winstreak: int = Field(alias="current_winstreak_mode_bridge_duel")
-    bow_shots: int = Field(alias="bridge_duel_bow_shots")
-    bow_hits: int = Field(alias="bridge_duel_bow_hits")
-    damage_dealt: int = Field(alias="bridge_duel_damage_dealt")
-    deaths: int = Field(alias="bridge_duel_bridge_deaths")
-    health_regenerated: int = Field(alias="bridge_duel_health_regenerated")
-    losses: int = Field(alias="bridge_duel_losses")
-    melee_hits: int = Field(alias="bridge_duel_melee_hits")
-    melee_swings: int = Field(alias="bridge_duel_melee_swings")
-    games_played: int = Field(alias="bridge_duel_rounds_played")
-    best_winstreak: int = Field(alias="best_winstreak_mode_bridge_duel")
-    kills: int = Field(alias="bridge_duel_bridge_kills")
-    wins: int = Field(alias="bridge_duel_wins")
-    blocks_placed: int = Field(alias="bridge_duel_blocks_placed")
-    goals: int = Field(alias="bridge_duel_goals")
+    _prefix: str = "bridge_duel_"
+    _suffix: str = "_mode_sumo_duel"
+    _game_mode: str = "sumo_duel"
 
-class SkyWarsDuelStats(BaseModel):
+    current_winstreak: int
+    bow_shots: int
+    bow_hits: int
+    damage_dealt: int
+    deaths: int = Field(alias="bridge_deaths")
+    health_regenerated: int
+    losses: int
+    melee_hits: int
+    melee_swings: int
+    games_played: int = Field(alias="rounds_played")
+    best_winstreak: int
+    kills: int = Field(alias="bridge_kills")
+    wins: int
+    blocks_placed: int
+    goals: int
+
+class SkyWarsDuelStats(StatsModel):
     """Skywars duel stats.
 
     Any stats to do with hearts are measured in halves.
     """
-    current_winstreak: int = Field(alias="current_winstreak_mode_sw_duel")
-    blocks_placed: int = Field(alias="sw_duel_blocks_placed")
-    damage_dealt: int = Field(alias="sw_duel_damage_dealt")
-    deaths: int = Field(alias="sw_duel_deaths")
-    health_regenerated: int = Field(alias="sw_duel_health_regenerated")
-    losses: int = Field(alias="sw_duel_losses")
-    melee_hits: int = Field(alias="sw_duel_melee_hits")
-    melee_swings: int = Field(alias="sw_duel_melee_swings")
-    games_played: int = Field(alias="sw_duel_rounds_played")
-    best_winstreak: int = Field(alias="best_winstreak_mode_sw_duel")
-    wins: int = Field(alias="sw_duel_wins")
-    kills: int = Field(alias="sw_duel_kills")
+    _prefix: str = "sw_duel_"
+    _suffix: str = "_mode_sw_duel"
+    _game_mode: str = "sw_duel"
+
+    current_winstreak: int
+    blocks_placed: int
+    damage_dealt: int
+    deaths: int
+    health_regenerated: int
+    losses: int
+    melee_hits: int
+    melee_swings: int
+    games_played: int = Field(alias="rounds_played")
+    best_winstreak: int
+    wins: int
+    kills: int
