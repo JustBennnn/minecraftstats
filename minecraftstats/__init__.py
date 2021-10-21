5e53c028bab2de397a5712645db52a4543b1a417
JustBennnnn <justben009@gmail.com>
2021-10-21 18:56:49 +0100

    removed unnecessary imports

--- minecraftstats/__init__.py
+++ minecraftstats/__init__.py
@@ -4,7 +4,5 @@ So far the user can get stats from the duels lobby.
 
 Go to the README.md for usage.
 """
-from .bedwarsstats import *
-from .duelstats import *
 from .errors import *
 from .minecraftstats import *
